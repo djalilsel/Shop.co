335544ec50c6a47b3e238a698a25c041afda93a3
Abd eldjalil <selamnia.a.eldjalil@gmail.com>
2023-08-23 12:38:00 +0100

    hero sections done

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,9 +1,49 @@
 
+import bg from '../assets/mainbg.png'
+import bgphone from '../assets/mainbgphone.png'
+import star from '../assets/star.png'
+
 export default function Home() {
+   
   return (
-    <main>
-      <span className="satoshi-400">High-Quality Products</span> <br />
-      <span className="integral">FIND CLOTHES THAT MATCHES YOUR STYLE</span>
+    <main className="sm:pt-32 p-4 pt-10 sm:h-[85vh] relative sm:bg-no-repeat sm:bg-cover sm:bg-center" style={{backgroundImage: `url(${bg.src})`}}>
+      <div className='sm:w-[40rem] flex flex-col gap-4 sm:gap-8'>
+        <h1 className='integral text-5xl sm:text-7xl no-selection'>
+          FIND CLOTHES <br />
+          THAT MATCHES <br />
+          YOUR STYLE <br />
+        </h1>
+          <p className='text-[#00000099] text-[17px]'>
+          Browse through our diverse range of meticulously crafted garments, 
+          designed to bring out your individuality and cater to your sense 
+          of style.
+        </p>
+        <div className='bg-darkblack satoshi-400 rounded-[3.875rem] flex justify-center sm:inline sm:w-fit text-basewhite text-lg py-[1rem] px-[4.5rem] cursor-pointer'>
+            Shop Now
+        </div>
+      </div>
+      <div>
+        <div className='flex justify-center sm:gap-24 mt-10'>
+          <div className='px-8 border-r border-[#00000030]'>
+            <div className='satoshi-700 text-[#000] text-4xl sm:text-6xl'>200+</div>
+            <div className='satoshi-400 text-[#00000099] text-base sm:text-xl'>International Brands</div> 
+          </div>
+          <div className='px-8 border-l border-[#00000030]'>
+            <div className='satoshi-700 text-[#000] text-4xl sm:text-6xl'>2,000+</div>
+            <div className='satoshi-400 text-[#00000099] text-base sm:text-xl'>High-Quality Products</div>
+          </div>
+        </div>
+        <div className='flex justify-center mt-6'>                
+          <div>
+            <div className='satoshi-700 text-[#000] text-4xl sm:text-6xl'>30,000+</div>
+            <div className='satoshi-400 text-[#00000099] text-base sm:text-xl'>Happy Customers</div>
+          </div>
+        </div>
+        <img src={star.src} className='hidden sm:absolute top-32 right-56 w-24 h-24 no-selection'/>
+        <img src={star.src} className='hidden sm:absolute top-96 right-[43%] w-20 h-20 no-selection'/>
+        <div className='mx-[-1rem]'>
+          <img src={bgphone.src} className=' w-full'/>
+        </div>
+      </div>
     </main>
-  )
-}
+  )}
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -11,14 +11,14 @@ const Nav = () => {
         <div className='flex flex-col '>
             <div id='signUp' className='px-4 sm:px-32 bg-darkblack text-[0.75rem] sm:text-[0.875rem] h-[2.375rem] w-full flex justify-center items-center text-basewhite '>
                 <div>Sign up and get 20% off to your first order. <Link className='font-semibold underline underline-offset-2 cursor-default' href='/signup'>Sign Up Now</Link></div>
-                <div className='hidden sm:inline sm:absolute right-32 text-[1rem] cursor-pointer' onClick={() => document.querySelector('#signUp').style.display = "none"}>X</div>
+                <div className='hidden no-selection sm:inline sm:absolute right-32 text-[1rem] cursor-pointer' onClick={() => document.querySelector('#signUp').style.display = "none"}>X</div>
             </div>
             <div className='h-20 sm:h-24 sm:px-32 px-4 w-full bg-basewhite flex gap-8 items-center boder-2 border-black'>
                 <FontAwesomeIcon icon={faBars} className='fa-lg lg:hidden'/>
-                <span className='integral sm:text-[2rem] text-2xl'>
+                <span className='integral sm:text-[2rem] text-2xl no-selection'>
                     SHOP.CO
                 </span>
-                <nav className='hidden lg:flex justify-center items-center gap-8 satoshi-400'>
+                <nav className='hidden lg:flex justify-center items-center gap-8 satoshi-400 cursor-default'>
                     <span>
                         Shop
                     </span>
@@ -34,7 +34,7 @@ const Nav = () => {
                 </nav>
                 <div className='hidden sm:flex relative flex-1'>
                     <input type="text" placeholder='Search for products...' 
-                        className='satoshi w-full bg-[#F0F0F0] pl-[2.25rem] p-[0.75rem] rounded-[3.875rem]'
+                        className='satoshi no-selection w-full bg-[#F0F0F0] pl-[2.25rem] p-[0.75rem] rounded-[3.875rem]'
                     />
                     <FontAwesomeIcon icon={faSearch} className='text-[#000] absolute top-4 left-2 px-1' />
                 </div>
