e89abc2801a6e65fa12bf063b882a26aa2109626
Abd eldjalil <selamnia.a.eldjalil@gmail.com>
2023-08-31 14:34:57 +0100

    register with email done

--- client/app/signup/page.jsx
+++ client/app/signup/page.jsx
@@ -4,15 +4,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import { useState } from 'react';
 import { apple, facebook, google } from '../../assets';
+import axios from 'axios'
+import { useRouter } from 'next/navigation';
 
 const page = () => {
 
+    const router = useRouter()
+    const [loading, setLoading] = useState("CREATE AN ACCOUNT")
+    const [error, setError] = useState('')
     const [input, setInput] = useState({
         name: '',
         user: '',
         password: '',
         email: ''
     })
+
+    const register = async () => {
+        const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+        if (!input.email.match(validRegex)) {
+            document.getElementById('email').classList.add('border-[#bc4646]')
+            return setError("You need to enter a valid email")
+        }
+        if(input.name == '' || input.user == '' || input.password == '' || input.email == '') { 
+            return setError("fill all the fields!")
+        }
+        if(!document.getElementById('terms').checked) return setError("You need to agree to the terms of service")
+        setLoading("Loading")
+        try{
+            const data = await axios.post('http://localhost:8800/api/auth/register', input)
+            setLoading("User created")
+            setTimeout(() => {
+                router.push('/signin')
+            }, 1000);
+        } catch(err){
+            setLoading("Failed to create user")
+            setError(err.response.data);
+        }
+    }
     const showPass = () => {
         document.querySelector('#pass').type = "text"
         document.querySelector('#hideeye').classList.add('block')
@@ -30,7 +59,7 @@ const page = () => {
         setInput(prevInput => ({...prevInput, [e.target.name]: e.target.value}))
     }
     return (
-        <div className='w-screen h-screen flex justify-center items-center'>
+        <div className='w-screen h-screen flex flex-col justify-center items-center'>
             <div className='absolute top-0 left-0 z-40 w-full h-20 2xl:h-24 xl:px-32 px-4 flex justify-between gap-8 items-center border-b border-[#00000060]'>
                 <Link href='/home' className='integral sm:text-[2rem] text-2xl no-selection'>
                     SHOP.CO
@@ -49,29 +78,30 @@ const page = () => {
                 <div className='flex flex-col w-full satoshi-400'>
                     <div className='relative w-full'>
                         <input name='name' type="text" value={input.name} onChange={handleChange} className='border  p-3 pt-8 w-96 text-[#00000080]'/>
-                        <span className='absolute top-3 left-3 text-sm'>Full name</span>
+                        <span className='absolute top-3 left-3 text-sm cursor-default'>Full name</span>
                     </div>
                     <div className='relative w-full'>
                         <input name='user' type="text" value={input.user} onChange={handleChange} className='border p-3 pt-8 w-full text-[#00000080]' />
-                        <span className='absolute top-3 left-3 text-sm'>User</span>
+                        <span className='absolute top-3 left-3 text-sm cursor-default'>User</span>
                     </div>
                     <div className='relative w-full'>
                         <input id='pass' name='password' type="password" value={input.password} onChange={handleChange} className='border p-3 pt-8 w-full text-[#00000080]' />
-                        <span className='absolute top-3 left-3 text-sm'>Password</span>
+                        <span className='absolute top-3 left-3 text-sm cursor-default'>Password</span>
                         <FontAwesomeIcon id='eye' icon={faEye} className='text-darkblack absolute top-10 right-3' onClick={showPass}/>
                         <FontAwesomeIcon id='hideeye' icon={faEyeSlash} className='hidden text-darkblack absolute top-10 right-3' onClick={hidePass}/>
                     </div>
                     <div className='relative w-full'>
-                        <input name='email' type="email" value={input.email} onChange={handleChange} className='border p-3 pt-8 w-full text-[#00000080]' />
-                        <span className='absolute top-3 left-3 text-sm'>Email</span>
+                        <input id='email' name='email' type="email" value={input.email} onChange={handleChange} className='border p-3 pt-8 w-full text-[#00000080]' />
+                        <span className='absolute top-3 left-3 text-sm cursor-default'>Email</span>
                     </div>
                 </div>
+                {error && <span className='satoshi-400 text-[#bc4646] text-xl self-start my-[-22px]'>{error}</span>}
                 <div className='flex gap-3 text-[#00000080] self-start'>
-                    <input type='checkbox' className='accent-darkblack w-5 h-5'/>
+                    <input id='terms' type='checkbox' className='accent-darkblack w-5 h-5'/>
                     <span className='text-sm underline underline-offset-2'>I agree to the Terms of Service and Privacy Policy.</span>
                 </div>
-                <div className='flex justify-center w-full satoshi-700 border bg-darkblack text-basewhite py-3 px-6'>
-                    CREATE AN ACCOUNT
+                <div className='flex justify-center w-full satoshi-700 border bg-darkblack text-basewhite py-3 px-6 cursor-default' onClick={register}>
+                    {loading}
                 </div>
                 <span>OR</span>
                 <div className='flex gap-9'>
@@ -79,6 +109,9 @@ const page = () => {
                     <img src={apple.src} alt="apple" className='w-9 h-9'/>
                     <img src={facebook.src} alt="facebook" className='w-9 h-9'/>
                 </div>
+            </div>
+            <div className='justify-self-end'>
+                <div className='text-[#00000060]'>Shop.co © 2000-2023, All Rights Reserved</div>
             </div>
         </div>
     );
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,18 +4,39 @@ import bcrypt from 'bcrypt'
 export const registerUser = (req, res) => {
     const saltRounds = 10
     const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(req.body[0].password, salt);
+    const hash = bcrypt.hashSync(req.body.password, salt);
     const values = [
-        req.body[0].name,
-        req.body[0].user,
+        req.body.name,
+        req.body.user,
         hash,
-        req.body[0].email
+        req.body.email
     ]
-    const q = 'INSERT INTO users (name, user, password, email) VALUES (?)'
+    const qq = 'SELECT * FROM users WHERE name = ?'
 
-    db.query(q, [values], (err, data) => {
-        if(err) throw res.status(500).json(err)
-        res.status(200).json("user created")
+    db.query(qq, req.body.name, (err, data) => {
+        if(data.length > 0) return res.status(403).json("name already taken")
+
+        const qqq = 'SELECT * FROM users WHERE email = ?'
+
+        db.query(qqq, req.body.email, (err, data) => {
+            if(err) throw res.status(500).json(err)
+            if(data.length > 0) return res.status(403).json("Email already linked with an account, Try account recovery")
+
+            const qqqq = 'SELECT * FROM users WHERE user = ?'
+            
+            db.query(qqqq, req.body.user, (err, data) => {
+                if(err) throw res.status(500).json(err)
+                if(data.length > 0) return res.status(403).json("User already taken")
+                const q = 'INSERT INTO users (name, user, password, email) VALUES (?)'
+            
+            db.query(q, [values], (err, data) => {
+                if(err) throw res.status(500).json(err)
+                    res.status(200).json("user created")
+                })
+            })
+
+        })
+
     })
     
 }
